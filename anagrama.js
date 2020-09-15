134ca56080a7dd5dabaee4ef5dc2635a4beaec90
Alan <devaliant94@gmail.com>
2020-09-15 14:48:51 -0300

    anagrama completado

--- anagrama.js
+++ anagrama.js
@@ -1,3 +1,6 @@
+//Retorna las coincidencias de string2 que hay en string1 sin distinguir mayusculas o minusculas
 function anagrama(string1, string2){
-  
+  return string1.match(new RegExp('(\\w*' + string2 + '\\w*)','gi')).length;
 }
+
+console.log(anagrama('hola, que buena ola Laomir','OLA'));
