f8400faa9ace27fc67247b13596c55c6b0f04305
Alan <devaliant94@gmail.com>
2020-09-16 07:16:21 -0300

    anagrama completo

--- anagrama.js
+++ anagrama.js
@@ -1,6 +1,36 @@
-//Retorna las coincidencias de string2 que hay en string1 sin distinguir mayusculas o minusculas
-function anagrama(string1, string2){
-  return string1.match(new RegExp('(\\w*' + string2 + '\\w*)','gi')).length;
+//Funcion que ordena el string recibido por parámetro
+function sortString(string) {
+  return string.split('').sort().join('');
 }
 
-console.log(anagrama('hola, que buena ola Laomir','OLA'));
+//En caso de encontrar un anagrama entre stringA y StringB retorna true caso contrario false
+function anagrams(stringA, stringB) {
+  stringA = stringA.replace(/[^\w]/g, '').toLowerCase()
+  stringB = stringB.replace(/[^\w]/g, '').toLowerCase()
+  
+  return sortString(stringA) === sortString(stringB)
+}
+
+//Funcion Principal
+function matchesAndAnagrams(string1, string2){
+  //Longitud de string2 para ir contando luego el ultimo caracter
+  let lastCharacter = string2.length;
+  let result = 0;
+
+  //Recorro toda la cadena string1
+  for (let i = 0; i < string1.length; i++) {
+    //Corto la cadena string1 desde i a lastCharacter para ir comparando 
+    let subString1 = string1.substring(i,lastCharacter)
+    //Si hay coincidencia, sumo 1 a result
+    if( anagrams(subString1,string2) ){
+      result++;
+    }
+    //Mantengo la relacion, si a i le sumo 1 tambien a
+    lastCharacter++
+  }
+
+  return result;
+}
+
+
+console.log(matchesAndAnagrams('hola, que buena ola Laomir','AOL'));
